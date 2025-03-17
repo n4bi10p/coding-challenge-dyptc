58b76b6925509dfd624e3a1de4e39bd664526097
Nabil Shaikh <snabilshaikh186@gmail.com>
2025-03-17 12:08:00 +0000

    array challange
    Solved the Day-1 Challenge of finding the Kth largest element in the array

---
+++ Day-01/ch-01.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+// Function to swap two numbers
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Function to sort array in descending order
+void sortDescending(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] < arr[j + 1]) {
+                swap(&arr[j], &arr[j + 1]);
+            }
+        }
+    }
+}
+
+// Function to find the Kth largest element
+int findKthLargest(int arr[], int n, int k) {
+    if (k > 0 && k <= n) {
+        sortDescending(arr, n);
+        return arr[k - 1]; // Kth largest element
+    }
+    return -1; // Invalid K
+}
+
+int main() {
+    int arr[] = {1, 4, 3, 8, 4, 9, 2, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int k = 3; // Example: Find 3rd largest element
+    
+    int result = findKthLargest(arr, n, k);
+    if (result != -1) {
+        printf("The %dth largest element is %d\n", k, result);
+    } else {
+        printf("Invalid K value\n");
+    }
+    
+    return 0;
+}
