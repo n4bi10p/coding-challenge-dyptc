83b237f47247213eda900bb74fe83c176d60701c
Nabil Shaikh <snabilshaikh186@gmail.com>
2025-03-19 09:25:34 +0000

    factorial of number
    Solved the Day-4 Challenge - program to find the factorial of a number using recursion.

---
+++ Day-04/ch-04.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// Function to calculate factorial using recursion
+unsigned long long factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+int main() {
+    int num;
+
+    // Taking input from the user
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    if (num < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+    } else {
+        printf("Factorial of %d is %llu\n", num, factorial(num));
+    }
+
+    return 0;
+}
